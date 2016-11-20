b3fe3ea50af80baaa6cf4cf4a5c90b7fe7b89eaf
perttu <savolainen.perttu@gmail.com>
2016-11-20 20:40:29 +0200

    tweaking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  scrollTimeout: any;
+
+  constructor(){
+    window.addEventListener('scroll', this.scrollEvent);
+  }
+
+  scrollEvent(e: Event){
+
+    clearTimeout(this.scrollTimeout);
+    this.scrollTimeout = setTimeout(()=>{
+
+    }, 100);
+
+  }
+
+  scrollDown(e: Event) {
+
+    let section = document.querySelector("section.front");
+    window.scrollTo(0, section.clientHeight);
+  }
+
 }

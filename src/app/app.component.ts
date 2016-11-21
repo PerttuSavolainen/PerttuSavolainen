1fe1d9746f3d072315ae21d591756b353d8b60bf
perttu <savolainen.perttu@gmail.com>
2016-11-21 08:38:10 +0200

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   scrollTimeout: any;
 
   constructor(){
     window.addEventListener('scroll', this.scrollEvent);
+
+
+
+  }
+
+  ngOnInit(){
+    // place static height to front section,
+    // it prevents content jumping on mobile devices
+    let section = document.querySelector("section.front");
+    section.setAttribute("style", "height: " + section.clientHeight + "px");
   }
 
   scrollEvent(e: Event){

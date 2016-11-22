81302b5b34ae1c72f1065269549752a90adb52ad
perttu <savolainen.perttu@gmail.com>
2016-11-22 07:32:25 +0200

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,6 +21,20 @@ export class AppComponent implements OnInit {
     // it prevents content jumping on mobile devices
     let section = document.querySelector("section.front");
     section.setAttribute("style", "height: " + section.clientHeight + "px");
+
+    // animate characters on at a time
+    let logoSpans = document.querySelectorAll(".logo-wrapper h1 span:not(.name)");
+    setTimeout(() => {
+      for (let i=0; i<logoSpans.length; i++) {
+        setTimeout(() => {
+          logoSpans[i].classList.add("manual-hover");
+          setTimeout(() => {
+            logoSpans[i].classList.remove("manual-hover");
+          }, (i+1)*50);
+        }, i*50);
+      }
+    }, 2000);
+
   }
 
   scrollEvent(e: Event){
@@ -35,7 +49,27 @@ export class AppComponent implements OnInit {
   scrollDown(e: Event) {
 
     let section = document.querySelector("section.front");
-    window.scrollTo(0, section.clientHeight);
+    //window.scrollTo(0, section.clientHeight);
+    //section.scrollIntoView({block: 'end', behavior: 'smooth'});
+
+    let scrollPos  = window.pageYOffset || document.documentElement.scrollTop;
+
+    this.scrollToEvent(scrollPos, section.clientHeight, 400);
+
   }
+
+  scrollToEvent(from: number, to: number, duration: number, currentTime: number = 0) {
+    if (from <= 0) { from = 0;}
+    if (to <= 0) { to = 0;}
+    if (currentTime>=duration) return;
+    let delta = to-from;
+    let progress = currentTime / duration * Math.PI / 2;
+    let position = delta * (Math.sin(progress));
+    setTimeout(() => {
+      document.body.scrollTop = from + position;
+      this.scrollToEvent(from, to, duration, currentTime + 10);
+    }, 10);
+  }
+
 
 }
